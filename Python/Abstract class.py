053b4c9445df7569a7a78aa91e14baa2070128c4
Kashish <98873675+Kashish5503@users.noreply.github.com>
2022-03-25 20:33:28 +0530

    Update Abstract class.py

--- Python/Abstract class.py
+++ Python/Abstract class.py
@@ -1,36 +1,37 @@
 from abc import ABC, abstractmethod
  
-class Polygon(ABC):
+class Polygon(ABC): #base class / super class
  
     @abstractmethod
     def noofsides(self):
         pass
  
-class Triangle(Polygon):
+class Triangle(Polygon): #subclass
  
     # overriding abstract method
     def noofsides(self):
         print("Triangle: I have 3 sides")
  
-class Pentagon(Polygon):
+class Pentagon(Polygon): #subclass
  
     # overriding abstract method
     def noofsides(self):
         print("Pentagon: I have 5 sides")
  
-class Hexagon(Polygon):
+class Hexagon(Polygon): #subclass
  
     # overriding abstract method
     def noofsides(self):
         print("Hexagon: I have 6 sides")
  
-class Quadrilateral(Polygon):
+class Quadrilateral(Polygon): #subclass
  
     # overriding abstract method
     def noofsides(self):
         print("Quadrilateral: I have 4 sides")
  
 # Driver code
+# Creating the objects to call the abstract method.
 R = Triangle()
 R.noofsides()
  
